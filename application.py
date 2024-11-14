e4477e417095b561e011760219e76e79bf3c1594
Ubuntu <ubuntu@ip-172-31-20-182.ec2.internal>
2024-11-14 10:19:27 +0000

    app and template files updated

--- application.py
+++ application.py
@@ -19,4 +19,4 @@ def loan_approval():
     return render_template('home.html', result=None)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0" debug=True, port=5000 )
+    app.run(host="0.0.0.0", debug=True, port=5000 )
